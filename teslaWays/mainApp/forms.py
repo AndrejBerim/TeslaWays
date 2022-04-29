639a88c61bdab096b6c6efd9d3ed9546f81ffa8d
Andrej <drejtool@gmail.com>
2022-04-29 11:26:52 +0200

    'folder_pics_added'

---
+++ teslaWays/mainApp/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from .models import Member
+
+
+class MemberForm(ModelForm):
+    class Meta:
+        model = Member
+        fields = '__all__'
+        exclude = ['password']
--- teslaWays/mainApp/views.py
+++ teslaWays/mainApp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def main_page(request):
     context = {}
     return render(request, 'main.html', context)
+
+
+def all_news(request):
+    all_news = News.objects.all()
+
+    context = {
+        'all_news': all_news,
+
+    }
+    return render(request, 'novosti.html', context)
