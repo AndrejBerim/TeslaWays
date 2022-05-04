bbd538512aafcdebf4e78b4507615b23bf6903f8
Andrej <drejtool@gmail.com>
2022-05-04 11:18:14 +0200

    commit

--- teslaWays/mainApp/views.py
+++ teslaWays/mainApp/views.py
@@ -5,18 +5,13 @@ from .models import *
 
 def main_page(request):
     context = {}
-<<<<<<< HEAD
-    return render(request, 'pocetna.html', context)
-=======
     return render(request, 'main.html', context)
 
 
 def all_news(request):
-    all_news = News.objects.all()
+    all_news = News.objects.all().values()
 
     context = {
         'all_news': all_news,
-
     }
     return render(request, 'novosti.html', context)
->>>>>>> 6b72777ce403536020efcfae117794c90bb374b0
