cfb3e10461c6e658f01742cc1b4a44cfa4769665
TijanaJelic <tijanajelic.jelic@gmail.com>
2022-05-17 11:21:04 +0200

    slider and map

--- teslaWays/static/js/map.js
+++ teslaWays/static/js/map.js
@@ -24,48 +24,81 @@ const hotels = [{
     lng: 19.97314
 }]
 
+const sights = [{
+    name: "Hilton",
+    lat: 44.0,
+    lng: 20.0
+},{
+    name: "Belgrade Inn",
+    lat: 42.817981,
+    lng: 21.462739
+},{
+    name: "Garni Hotel",
+    lat: 40.819868,
+    lng: 20.4619026
+},{
+    name: "Cricket 2",
+    lat: 44.822494,
+    lng: 21.4626116
+},{
+    name: "Novi Sad",
+    lat: 45.23621,
+    lng: 13.97314
+}]
+
+var map
+var markerGroup
 const showMap = (markers = null) => {
-    const mapOptions = {
-        center: [44.453, 20.083],
-        zoom: 7
-    }
     
-    const map = new L.map('map', mapOptions)
+    if (!map) {
+        map = new L.map('map', {
+            center: [44.453, 20.083],
+            zoom: 7
+        })
+    }
+
+    markerGroup = L.layerGroup().addTo(map)
 
     if (markers) {
         for (let i = 0; i < markers.length; i++) {
-            L.marker(markers[i]).addTo(map)
+            var marker = L.marker(markers[i]).addTo(markerGroup)
+            marker.bindPopup(`${markers[i][2]}`)
         }
+        var bounds = new L.LatLngBounds(markers)
+        map.fitBounds(bounds)
     }
     
     const layer = new L.TileLayer(`http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`);
-    map.addLayer(layer);   
-    
+    map.addLayer(layer)
+    map.addLayer(markerGroup)
 }
 
 const showHotels = () => {
     const allHotels = []
     for (let i = 0; i < hotels.length; i++) {
-        const hotel = [hotels[i].lat, hotels[i].lng]
+        const hotel = [hotels[i].lat, hotels[i].lng, hotels[i].name]
         allHotels.push(hotel)
     }
+    markerGroup.clearLayers()
     showMap(allHotels)
+}
+
+const showSights = () => {
+    const allSights = []
+    for (let i = 0; i < sights.length; i++) {
+        const sight = [sights[i].lat, sights[i].lng, sights[i].name]
+        allSights.push(sight)
+    }
+    markerGroup.clearLayers()
+    showMap(allSights)
 }
 
 hotelsEl.addEventListener('click', () => {
     showHotels()
 })
 
+sightsEl.addEventListener('click', () => {
+    showSights()
+})
+
 showMap()
-
-
-// const mapOptions = {
-//     center: [44.80446, 20.46578],
-//     zoom: 15
-// }
-    
-// const map = new L.map('map', mapOptions);
-// const marker = L.marker([44.80446, 20.46578]).addTo(map);
-// const layer = new L.TileLayer(`http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`);
-
-// map.addLayer(layer);
---
+++ teslaWays/static/js/navigation.js
@@ -0,0 +1,15 @@
+// Navigation
+
+const navCities = document.querySelectorAll('.city')
+const lists = document.querySelectorAll('.sec-menu ul')
+
+navCities.forEach(city => {
+    city.addEventListener('click', () => {
+        lists.forEach(list => {
+            if(list.classList.contains('show')) {
+                list.classList.remove('show')
+            }
+        })
+        city.nextElementSibling.classList.toggle('show')
+    })
+})
