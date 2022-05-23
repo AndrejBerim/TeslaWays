36f444b91cbf340b7f5a9f018d931cdf81662e64
Andrej <drejtool@gmail.com>
2022-05-23 12:12:04 +0200

    commit

--- teslaWays/map/models.py
+++ teslaWays/map/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from multiselectfield import MultiSelectField
+from mainApp.models import *
 
 
 # Create your models here.
--- teslaWays/teslaWays/urls.py
+++ teslaWays/teslaWays/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
-    path('api-auth/', include('api.urls')),
+    path('api/', include('api.urls')),
     path('map/', include('map.urls')),
 
 ]
